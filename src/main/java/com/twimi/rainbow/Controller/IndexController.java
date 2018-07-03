b056e3b389e6454e8e0a9b0303d09a60818abd38
IvanLuLyf <835498692@qq.com>
2018-07-03 15:33:07 +0800

    添加了个页面

--- src/main/java/com/twimi/rainbow/Controller/IndexController.java
+++ src/main/java/com/twimi/rainbow/Controller/IndexController.java
@@ -1,13 +1,16 @@
 package com.twimi.rainbow.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 public class IndexController {
     @RequestMapping("/index")
-    public String index(){
-        return "内心永远是彩虹色";
+    public String index(ModelMap modelMap){
+        modelMap.addAttribute("title","Hello World");
+        modelMap.addAttribute("message","内心永远是彩虹色");
+        return "index";
     }
 }
